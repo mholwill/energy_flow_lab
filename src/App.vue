6e98ad7c4e44f384fa87f9a02018023552ec22c6
mholwill <m_holwill@hotmail.co.uk>
2020-02-27 12:02:00 +0000

    FormattedFuels done for chart data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <h1>Energy Data</h1>
+    <h2>{{this.formattedFuels}}</h2>
+
   </div>
 </template>
 
@@ -11,13 +13,29 @@ export default {
   name: 'App',
   data() {
     return {
-      energyTypes: {}
+      energyTypes: {},
+      formattedFuels: []
+    }
+  },
+  methods: {
+    formatFuel: function(fuelObjects) {
+      fuelObjects.forEach((fuelObject) => {
+        let newArray = []
+        newArray.push(fuelObject.fuel)
+        newArray.push(fuelObject.perc)
+        this.formattedFuels.push(newArray)
+      })
     }
   },
   mounted() {
   fetch('https://api.carbonintensity.org.uk/generation')
   .then(res => res.json())
-  .then(energyTypes => this.energyTypes = energyTypes)
+  .then((energyTypes) => {
+    this.energyTypes = energyTypes;
+    this.formatFuel(energyTypes.data.generationmix);
+    console.log(this.energyTypes);
+    console.log(this.formattedFuels);
+  })
   }
 }
 
---
+++ src/components/FuelsChart.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: "fuel-list"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
